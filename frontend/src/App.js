0a6d29ffd5efacc414551a18df808363510a1af7
Amrit Anand Singh <50056654+amritanandsingh@users.noreply.github.com>
2024-02-04 23:38:30 +0530

    google Auth0 is implement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
+import React from "react";
 import Headers from "./main/header";
 import Body from "./main/body";
-function App ()  {
+import { useAuth0 } from "@auth0/auth0-react";
+
+function App() {
+  const { user, loginWithRedirect, isAuthenticated, logout } = useAuth0();
+  console.log("current user logged in", user);
 
   return (
-    <div className="container"> 
-    <Headers/>
-    <Body/>
-    </div> 
+    <div className="container">
+      <Headers />
+      {isAuthenticated && <h3>Hello {user.name}</h3>}
+      {isAuthenticated && <Body />}
+
+      {isAuthenticated ? (
+        <button onClick={(e) => logout()}>Log Out</button>
+      ) : (
+        <button onClick={(e) => loginWithRedirect()}>LoginWithRedirect</button>
+      )}
+    </div>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-7d0ygutb8yledbnb.us.auth0.com"
+      clientId="sr90NTWpUqYxMBupqN2L8HRDz4rDbAyM"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+      >
+      <App />
+    </Auth0Provider>
+    
   </React.StrictMode>
 );
 
